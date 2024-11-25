7fdd1145858cbcd993c54464ce167ec8f4bdd210
Sumalatha Siddareddy <40268998+sumalathasiddareddy@users.noreply.github.com>
2024-11-25 00:09:36 -0800

    Time: 7 ms (97.71%) | Memory: 17.2 MB (6.94%) - LeetSync

--- 649-dota2-senate/dota2-senate.py
+++ 649-dota2-senate/dota2-senate.py
@@ -3,12 +3,18 @@ class Solution:
        
         n= len(senate)
 
-        rInd = [i for i in range(n) if senate[i]=='R']
-        dInd = [i for i in range(n) if senate[i]=='D']
+        rInd = deque()
+        dInd = deque()
+
+        for i in range(n):
+            if senate[i]=='D':
+                dInd.append(i)
+            else:
+                rInd.append(i)
 
         while rInd and dInd:
-            r=rInd.pop(0)
-            d=dInd.pop(0)
+            r=rInd.popleft()
+            d=dInd.popleft()
             if r<d:
                 rInd.append(n+r)
             else:
