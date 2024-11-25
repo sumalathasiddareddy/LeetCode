a813422ba17f13037e52d4845b93f1fd8f3f581f
Sumalatha Siddareddy <40268998+sumalathasiddareddy@users.noreply.github.com>
2024-11-25 00:07:32 -0800

    Time: 63 ms (27.69%) | Memory: 17.3 MB (6.94%) - LeetSync

--- 649-dota2-senate/dota2-senate.py
+++ 649-dota2-senate/dota2-senate.py
@@ -1,41 +1,17 @@
 class Solution:
     def predictPartyVictory(self, senate: str) -> str:
-        
-        senators = deque()
-        
-        while True:
-            
-            unvotedR=0
-            unvotedD=0
-            newstr=""
+       
+        n= len(senate)
 
-            if (senate==len(senate)*'R') or (senate==len(senate)*'D'):
-                break
-            
-            for char in senate:
-                if char=='D':
-                    if unvotedR==0:
-                        senators.appendleft(char)
-                        unvotedD+=1
-                    else:
-                        unvotedR-=1
-                elif char=='R':
-                    if unvotedD==0:
-                        senators.appendleft(char)
-                        unvotedR+=1
-                    else:
-                        unvotedD-=1
-                
-            for char in ''.join(senators)[::-1]:
-                if char=='R' and unvotedD>0:
-                    unvotedD-=1
-                    continue
-                if char=='D' and unvotedR>0:
-                    unvotedR-=1
-                    continue
-                newstr += char
-            
-            senate=newstr
-            senators.clear()
-                    
-        return "Radiant" if senate[0]=='R'else "Dire"
+        rInd = [i for i in range(n) if senate[i]=='R']
+        dInd = [i for i in range(n) if senate[i]=='D']
+
+        while rInd and dInd:
+            r=rInd.pop(0)
+            d=dInd.pop(0)
+            if r<d:
+                rInd.append(n+r)
+            else:
+                dInd.append(n+d)
+           
+        return "Radiant" if len(rInd)>0 else "Dire"
