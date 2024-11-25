ecc1b6c3c139f728bfbfc9c81bf1f38aca67b3ec
Sumalatha Siddareddy <40268998+sumalathasiddareddy@users.noreply.github.com>
2024-11-25 15:17:19 -0800

    Time: 970 ms (10.28%) | Memory: 24.7 MB (72.00%) - LeetSync

--- 189-rotate-array/rotate-array.py
+++ 189-rotate-array/rotate-array.py
@@ -5,8 +5,9 @@ class Solution:
         """
 
         n = len(nums)
+        
         for i in range(k):#O(k)
-            m=nums[-1] 
+            m=nums[-1]
             nums.pop()  #O(1)
             nums.insert(0,m) #O(n)
         
