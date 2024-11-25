fb441b47d26f2ac7c5a5c3318aa02f3e2168ed1c
Sumalatha Siddareddy <40268998+sumalathasiddareddy@users.noreply.github.com>
2024-11-25 14:47:43 -0800

    Time: 7 ms (57.84%) | Memory: 18.5 MB (15.72%) - LeetSync

--- 169-majority-element/majority-element.py
+++ 169-majority-element/majority-element.py
@@ -1,6 +1,16 @@
 class Solution:
     def majorityElement(self, nums: List[int]) -> int:
-        nums.sort()
-        return nums[len(nums)//2]
+        #nums.sort()
+        #return nums[len(nums)//2]
+
+        counter = Counter(nums)
+        maxV=0
+        for k,v in counter.items():
+            if v>maxV:
+                maxV = v
+                key = k
+
+        return key    
+
        
         
