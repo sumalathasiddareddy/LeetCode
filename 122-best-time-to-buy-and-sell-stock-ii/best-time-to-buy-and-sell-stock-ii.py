5f447fc2f403823e69c3fb1a4f2b17c51c6d1cab
Sumalatha Siddareddy <40268998+sumalathasiddareddy@users.noreply.github.com>
2024-11-26 13:50:46 -0800

    Time: 0 ms (100.00%) | Memory: 18.6 MB (7.22%) - LeetSync

---
+++ 122-best-time-to-buy-and-sell-stock-ii/best-time-to-buy-and-sell-stock-ii.py
@@ -0,0 +1,18 @@
+class Solution:
+    def maxProfit(self, prices: List[int]) -> int:
+
+        n=len(prices)
+        minPrice=sys.maxsize
+        totalProfit=0
+
+        for i in range(n):
+            if prices[i] < minPrice:
+                minPrice = prices[i]
+            else:
+                totalProfit += prices[i]-minPrice
+                minPrice = prices[i]
+
+        return totalProfit
+
+
+        
