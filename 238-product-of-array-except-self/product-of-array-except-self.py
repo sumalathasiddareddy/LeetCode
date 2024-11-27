e50933ab62e30b8ea7c55ecd83ee116da3922c3a
Sumalatha Siddareddy <40268998+sumalathasiddareddy@users.noreply.github.com>
2024-11-27 12:43:56 -0800

    Time: 19 ms (85.80%) | Memory: 24.9 MB (58.77%) - LeetSync

--- 238-product-of-array-except-self/product-of-array-except-self.py
+++ 238-product-of-array-except-self/product-of-array-except-self.py
@@ -4,22 +4,22 @@ class Solution:
         n=len(nums)
         answer=[1]*n
         prefix=[1]*n
-        suffix=[1]*n
-      
+             
         prefix[0]=1
-        suffix[n-1]=1
-        
+               
         for i in range(1,n):
             prefix[i] = prefix[i-1]*nums[i-1]
         
-        for j in range(n-2,-1,-1):
-            suffix[j] = suffix[j+1]*nums[j+1]
-
-        for k in range(n):
-            answer[k] = prefix[k]*suffix[k]
+        temp=1
+        for j in range(n-1,-1,-1):
+            if j==0:
+                preProd=1
+            else:
+                preProd=prefix[j]
+            answer[j] = preProd*temp
+            temp*=nums[j]
 
         return answer
-
 
 
         
