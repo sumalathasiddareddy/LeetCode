9f920dff8d1f5454adb14bb07f1d78b03e8758ef
Sumalatha Siddareddy <40268998+sumalathasiddareddy@users.noreply.github.com>
2024-11-28 15:57:37 -0800

    Time: 0 ms (100.00%) | Memory: 17.3 MB (15.40%) - LeetSync

--- 58-length-of-last-word/length-of-last-word.py
+++ 58-length-of-last-word/length-of-last-word.py
@@ -1,5 +1,12 @@
 class Solution:
     def lengthOfLastWord(self, s: str) -> int:
 
-        return len(s.strip().split(" ")[-1])
+        #return len(s.strip().split(" ")[-1])
+
+        s=s.strip()
+        i =len(s)-1
+        while i>=0 and s[i]!=" ":
+            i-=1
+        
+        return len(s)-i-1 if i>=0 else len(s)
         
