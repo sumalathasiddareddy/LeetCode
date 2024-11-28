76b88cbbe80b31de3d7e4d69ea47c543863c5b73
Sumalatha Siddareddy <40268998+sumalathasiddareddy@users.noreply.github.com>
2024-11-28 15:33:23 -0800

    Time: 7 ms (44.12%) | Memory: 17.5 MB (7.43%) - LeetSync

--- 12-integer-to-roman/integer-to-roman.py
+++ 12-integer-to-roman/integer-to-roman.py
@@ -1,20 +1,7 @@
 class Solution:
     def intToRoman(self, num: int) -> str:
-        d={
-        4:"IV",
-        9:"IX" ,
-        40:"XL",
-        90:"XC",
-        400:"CD", 
-        900:"CM",
-        1:"I",
-        5:"V",
-        10:"X",
-        50:"L",
-        100:"C",
-        500:"D",
-        1000:"M"
-        }
+        
+        d={4:"IV",9:"IX" ,40:"XL",90:"XC",400:"CD", 900:"CM",1:"I",5:"V",10:"X",50:"L",100:"C",500:"D",1000:"M"}
 
         romanStr=""
         placeValue=1
