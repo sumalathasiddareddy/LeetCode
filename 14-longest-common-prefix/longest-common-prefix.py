ed300bd946c59755af65748b912fe3e00ec5c8b7
Sumalatha Siddareddy <40268998+sumalathasiddareddy@users.noreply.github.com>
2024-11-28 16:14:54 -0800

    Time: 0 ms (100.00%) | Memory: 17.3 MB (5.71%) - LeetSync

--- 14-longest-common-prefix/longest-common-prefix.py
+++ 14-longest-common-prefix/longest-common-prefix.py
@@ -3,10 +3,12 @@ class Solution:
 
             prefix=""
             end=False
+            n1=len(strs)
+            n2=len(strs[0])
 
-            for j in range(len(strs[0])):
+            for j in range(n2):
                 char= strs[0][j]
-                for i in range(len(strs)):
+                for i in range(n1):
                     if j>=len(strs[i]) or strs[i][j] !=char:
                         end=True
                         break
