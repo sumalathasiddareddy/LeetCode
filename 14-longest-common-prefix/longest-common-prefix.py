289756420999e65ca44122802740fbda6e0a9040
Sumalatha Siddareddy <40268998+sumalathasiddareddy@users.noreply.github.com>
2024-11-28 16:34:20 -0800

    Time: 0 ms (100.00%) | Memory: 17.4 MB (5.71%) - LeetSync

--- 14-longest-common-prefix/longest-common-prefix.py
+++ 14-longest-common-prefix/longest-common-prefix.py
@@ -1,6 +1,19 @@
 class Solution:
     def longestCommonPrefix(self, strs: List[str]) -> str:
-            '''My solution
+            
+            prefix=""
+            strs.sort()
+            n = min(len(strs[0]),len(strs[-1]))            
+
+            for i in range(n):
+                if strs[0][i]!=strs[-1][i]:
+                    return prefix
+                prefix+=strs[0][i]
+            
+            return prefix
+
+            '''My solution->
+            
             prefix=""
             end=False
             n1=len(strs)
@@ -15,14 +28,3 @@ class Solution:
                 
             return prefix
             '''
-
-            prefix=""
-            strs.sort()
-            n = min(len(strs[0]),len(strs[-1]))            
-
-            for i in range(n):
-                if strs[0][i]!=strs[-1][i]:
-                    return prefix
-                prefix+=strs[0][i]
-            
-            return prefix
