c6e8faf13d31239d78928d0337ccd581820ba485
Sumalatha Siddareddy <40268998+sumalathasiddareddy@users.noreply.github.com>
2024-11-30 12:49:33 -0800

    Time: 7 ms (81.42%) | Memory: 22.7 MB (5.18%) - LeetSync

---
+++ 125-valid-palindrome/valid-palindrome.py
@@ -0,0 +1,8 @@
+class Solution:
+    def isPalindrome(self, s: str) -> bool:
+       
+        news=''.join([char.lower() for char in s if char.isalnum()])
+        return news==news[::-1]
+
+
+       
