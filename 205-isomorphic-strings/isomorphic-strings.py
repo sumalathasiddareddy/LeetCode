a5d72256c5fa47805405343d2a411416791ff5c2
Sumalatha Siddareddy <40268998+sumalathasiddareddy@users.noreply.github.com>
2024-12-06 20:41:53 -0800

    Time: 4 ms (76.78%) | Memory: 17.6 MB (6.52%) - LeetSync

--- 205-isomorphic-strings/isomorphic-strings.py
+++ 205-isomorphic-strings/isomorphic-strings.py
@@ -1,17 +1,18 @@
 class Solution:
     def isIsomorphic(self, s: str, t: str) -> bool:
         
-        news=""
         d={}
-        c1=Counter(s)
-        c2=Counter(t)
-        
         for i in range(len(s)):
-            if s[i] not in d:
-                d[s[i]]=t[i]
-                news+=t[i]
+            if s[i] in d:
+                if d[s[i]] != t[i]:
+                    return False
             else:
-                news+=d[s[i]]
+                if t[i] in d.values():
+                    return False
+                else:
+                    d[s[i]] = t[i]
+        
+        return True
 
-        return t==news and (len(c1.keys()) == len(c2.keys()) and sorted(list(c1.values()))==sorted(list(c2.values())))
 
+      
